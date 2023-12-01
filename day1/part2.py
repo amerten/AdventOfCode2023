e38426b32d8248f4301fd62bb2b2473490972bb2
amerten <adrien_mertens@hotmail.com>
2023-12-01 10:05:13 +0100

    Day 1 - cleaner solution ?

--- day1/part2.py
+++ day1/part2.py
@@ -1,25 +1,31 @@
 with open('input.txt') as f:
     data = [line.strip() for line in f.readlines()]
 
+numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+
+def real_value(val):
+    conversion = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+    if val in conversion:
+        return conversion[val]
+    if val[::-1] in conversion:
+        return conversion[val[::-1]]
+    return val
+
+
+def find_first(line, reverse=False):
+    if reverse:
+        line = line[::-1]
+    for i, c in enumerate(line):
+        for n in numbers:
+            if reverse:
+                n = n[::-1]
+            if n == c or (n in line and line.index(n) == i):
+                return real_value(n)
+
 
 def calibration_value(line):
-    numbers = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-    v = [None, None]
-    for i, c in enumerate(line):
-        if c in numbers.values():
-            if not v[0] and not v[1]:
-                v[0] = c
-            else:
-                v[1] = c
-        for n in numbers:
-            if n in line[i:] and line.index(n, i) == i:
-                if not v[0] and not v[1]:
-                    v[0] = numbers[n]
-                else:
-                    v[1] = numbers[n]
-                break
-    if not v[1]:
-        return int(v[0] + v[0])
-    return int(v[0] + v[1])
+    return int(find_first(line) + find_first(line, True))
+
 
 print(sum(map(calibration_value, data)))
