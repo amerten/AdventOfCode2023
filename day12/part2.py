899c77b4177958451d4ed2c1225113ae0dcb2419
amerten <adrien_mertens@hotmail.com>
2023-12-12 08:59:04 +0100

    Day 12 - part 1

---
+++ day12/part2.py
@@ -0,0 +1,35 @@
+import itertools
+import re
+
+data = open(0).read().splitlines()
+
+
+def can_match(code, clues):
+    m = re.findall('#+', code)
+    # print(m)
+    # print(code)
+    # print(clues)
+    # print(all(len(m[i]) == clues[i] for i in range(min(len(m), len(clues)))))
+    # print()
+    return len(m) == len(clues) and all(len(m[i]) == clues[i] for i in range(len(clues)))
+
+def nb_arrangements(line):
+    code, clues = line.split()
+    clues = list(map(int, clues.split(",")))
+    nb_unknown = code.count('?')
+    nb_arrangements = 0
+    for p in itertools.product('.#', repeat=nb_unknown):
+        lcode = list(code)
+        new_code = []
+        for _ in range(5):
+
+        n = 0
+        for i, c in enumerate(lcode):
+            if c == '?':
+                lcode[i] = p[n]
+                n += 1
+        nb_arrangements += can_match(''.join(lcode), clues)
+    return nb_arrangements
+
+
+print(sum(map(nb_arrangements, data)))
