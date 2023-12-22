e6770f1bd4e7d00d5b15414ef2040c9a0556081e
amerten <adrien_mertens@hotmail.com>
2023-12-22 17:04:09 +0100

    Day 19 - part 2

--- day19/part2.py
+++ day19/part2.py
@@ -1,55 +1,63 @@
-w, r = open(0).read().split('\n\n')
-workflows, ratings = {}, []
+import re
+
+
+def opposite(r):
+    r = r.split(':')[0]
+    s = '<' if '<' in r else '>'
+    os = '<' if s == '>' else '>'
+    return r.split(s)[0] + os + "=" + r.split(s)[1]
+
+
+w, _ = open(0).read().split('\n\n')
+workflows = {}
 
 for workflow in w.splitlines():
     name, rest = workflow.split('{')
     workflows[name] = rest[:-1].split(',')
 
-for rating in r.splitlines():
-    x, m, a, s = map(lambda x: int(x.split('=')[1]), rating[1:-1].split(','))
-    ratings.append({'x': x, 'm': m, 'a': a, 's': s})
+ok_ranges = []
+
+
+def update_ranges(wf_name, ok_r):
+    rules = workflows[wf_name]
+    for i, rule in enumerate(rules):
+        previous_rules = ""
+        for r in rules[:i]:
+            previous_rules += opposite(r)
+        if rule == 'A':
+            ok_ranges.append(ok_r + previous_rules)
+        elif '<' in rule or '>' in rule:
+            dr, destination = rule.split(':')
+            if destination == 'A':
+                ok_ranges.append(ok_r + previous_rules + dr)
+            elif destination != 'R':
+                update_ranges(destination, ok_r + previous_rules + dr)
+        elif rule != 'R':
+            update_ranges(rule, ok_r + previous_rules)
+
+
+update_ranges('in', "")
+parts = ['x', 'm', 'a', 's']
 
 t = 0
-for rating in ratings:
-    wf = workflows['in']
-    done = False
-    while not done:
-        for i, rule in enumerate(wf):
-            if rule == 'A':
-                t += sum(rating.values())
-                done = True
-                break
-            elif rule == 'R':
-                done = True
-                break
-            elif '<' in rule:
-                comp, target = rule.split(':')
-                part, n = comp.split('<')[0], int(comp.split('<')[1])
-                if rating[part] < n:
-                    if target == 'A':
-                        t += sum(rating.values())
-                        done = True
-                        break
-                    elif target == 'R':
-                        done = True
-                        break
-                    else:
-                        wf = workflows[target]
-                        break
-            elif '>' in rule:
-                comp, target = rule.split(':')
-                part, n = comp.split('>')[0], int(comp.split('>')[1])
-                if rating[part] > n:
-                    if target == 'A':
-                        t += sum(rating.values())
-                        done = True
-                        break
-                    elif target == 'R':
-                        done = True
-                        break
-                    else:
-                        wf = workflows[target]
-                        break
-            else:
-                wf = workflows[rule]
+for ok_range in ok_ranges:
+    all_ranges = [[1, 4000], [1, 4000], [1, 4000], [1, 4000]]
+    p = "(x|m|a|s)([<>]=?)(\d+)"
+    m = re.findall(p, ok_range)
+    for part, sign, val in m:
+        i = parts.index(part)
+        if sign == '<':
+            all_ranges[i][1] = min(all_ranges[i][1], int(val) - 1)
+        elif sign == '<=':
+            all_ranges[i][1] = min(all_ranges[i][1], int(val))
+        elif sign == '>':
+            all_ranges[i][0] = max(all_ranges[i][0], int(val) + 1)
+        elif sign == '>=':
+            all_ranges[i][0] = max(all_ranges[i][0], int(val))
+        else:
+            raise Exception("oops")
+    sub_t = 1
+    for i in range(len(all_ranges)):
+        sub_t *= (all_ranges[i][1] - all_ranges[i][0] + 1)
+    t += sub_t
 print(t)
